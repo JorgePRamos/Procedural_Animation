26a795753e30edfb5131f8a6a81883b31d78f947
Jorge Pérez Ramos <jorgepramoscontact@gmail.com>
2023-11-30 17:36:11 +0100

    Improvement in stability
    Moved all model affecting logic to third_person_template.cs

--- Scripts/third_person_template.cs
+++ Scripts/third_person_template.cs
@@ -1,13 +1,19 @@
 using Godot;
 using System;
 public partial class third_person_template : CharacterBody3D
-{	
+{
 	//Exported variables so we can change them in editor
 	[Export(PropertyHint.Range, "0.1,1.0")] float verticalCamSensitivity = 0.7f;
 	[Export(PropertyHint.Range, "0.1,1.0")] float horizontalCamSensitivity = 0.3f;
 	[Export(PropertyHint.Range, "-90,0,1")] float minCamPitch = -50f;
 	[Export(PropertyHint.Range, "0,90,1")] float maxCamPitch = 30f;
 
+	[Export] float hightOffset = 0.5f;
+	Marker3D frontLIK;
+	Marker3D frontRIK;
+	Marker3D backLIK;
+	Marker3D backRIK;
+	CharacterBody3D charBody;
 	public const float Speed = 5.0f;
 	public const float JumpVelocity = 4.5f;
 
@@ -20,12 +26,16 @@ public partial class third_person_template : CharacterBody3D
 		base._Ready();
 		//Make mouse not visible and limit it to the game screen
 		Input.MouseMode = Input.MouseModeEnum.Captured;
+		frontLIK = GetNode<Node3D>("Visuals").GetNode<Node3D>("rob_model").GetNode<Marker3D>("Front_L_MK");
+		frontRIK = GetNode<Node3D>("Visuals").GetNode<Node3D>("rob_model").GetNode<Marker3D>("Front_R_MK");
+		backLIK = GetNode<Node3D>("Visuals").GetNode<Node3D>("rob_model").GetNode<Marker3D>("Back_L_MK");
+		backRIK = GetNode<Node3D>("Visuals").GetNode<Node3D>("rob_model").GetNode<Marker3D>("Back_R_MK");
 
 	}
 	//Accumulators
 	private float _rotationX = 0f;
 	private float _rotationY = 0f;
-	
+
 	//Mouse rotation
 	public override void _Input(InputEvent @event)
 	{
@@ -34,19 +44,38 @@ public partial class third_person_template : CharacterBody3D
 		Vector3 camRot = GetNode<Node3D>("Camera_Mount").RotationDegrees;
 
 		if (@event is InputEventMouseMotion mouseMotion)
-		{	
+		{
 			//Orbit character
 			//camRot.Y -= mouseMotion.Relative.X * camSensitivity;
 			camRot.X -= mouseMotion.Relative.Y * verticalCamSensitivity;
-	
+
 			RotateY(mathTools.DegToRad(mouseMotion.Relative.X * horizontalCamSensitivity));
-			
+
 		}
-		
+
 		//Prevents camera from going endlessly around the player
 		camRot.X = Mathf.Clamp(camRot.X, minCamPitch, maxCamPitch);
 		//Update Camera arm mount current rotation
 		GetNode<Node3D>("Camera_Mount").RotationDegrees = camRot;
+	}
+	public override void _Process(double delta)
+	{
+		base._Process(delta);
+		Plane plane1 = new Plane(backLIK.GlobalPosition, frontLIK.GlobalPosition, frontRIK.GlobalPosition);
+		Plane plane2 = new Plane(frontRIK.GlobalPosition, backRIK.GlobalPosition, backLIK.GlobalPosition);
+		Vector3 avgNormal = ((plane1.Normal + plane2.Normal) / 2).Normalized();
+
+		//charBody = GetNode<CharacterBody3D>("Player");
+		//float moveSpeed = (float)charBody.Get("Speed");
+		float moveSpeed = Speed;
+		Basis targetBasis = basisFromNormal(avgNormal);
+		Basis = Basis.Slerp(targetBasis, (float)(moveSpeed * delta)).Orthonormalized();
+
+		Vector3 avg = (frontLIK.Position + frontRIK.Position + backRIK.Position + backLIK.Position) / 4;
+		Vector3 target = avg + Basis.Y * hightOffset;
+		float distance = Basis.Y.Dot(target - Position);
+		//Position = Position.Slerp((Position + Transform.Basis.Y * distance).Normalized(), (float)(moveSpeed * delta)).Normalized();
+
 	}
 	public override void _PhysicsProcess(double delta)
 	{
@@ -77,6 +106,23 @@ public partial class third_person_template : CharacterBody3D
 
 		Velocity = velocity;
 		MoveAndSlide();
+	}
+
+	public Basis basisFromNormal(Vector3 normal)
+	{
+		Basis result = new Basis();
+
+		result.X = normal.Cross(Transform.Basis.Z);
+		result.Y = normal;
+		result.Z = Transform.Basis.X.Cross(normal);
+
+		result = result.Orthonormalized();
+		result.X *= Scale.X;
+		result.Y *= Scale.Y;
+		result.Z *= Scale.Z;
+
+
+		return result;
 	}
 }
 
